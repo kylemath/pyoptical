c45614ff8b001184d7099946b2edfdb31fc1ce62
Kyle Mathewson <kylemath@gmail.com>
2020-04-23 16:16:25 -0700

    other optoins

--- test3.py
+++ test3.py
@@ -8,7 +8,7 @@ data_path = mne.datasets.sample.data_path()
 subjects_dir = data_path + '/subjects'
 trans = mne.read_trans(data_path + '/MEG/sample/sample_audvis_raw-trans.fif')
 raw = mne.io.read_raw_fif(data_path + '/MEG/sample/sample_audvis_raw.fif')
-fig = plot_alignment(raw.info, trans, subject='sample', dig=True,
-                     eeg=['original', 'projected'], meg=[],
+fig = plot_alignment(raw.info, trans, subject='sample', dig=False,
+                     eeg=['projected'], meg=[],
                      coord_frame='head', subjects_dir=subjects_dir)
 set_3d_view(figure=fig, azimuth=135, elevation=80)
